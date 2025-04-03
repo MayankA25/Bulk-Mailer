53bca8fccd1447e2a86cd51af0a14a619b70c697
Mayank <mayank.a125052@gmail.com>
2025-04-03 23:12:34 +0530

    changes 6

--- server/src/index.js
+++ server/src/index.js
@@ -64,7 +64,7 @@ app.use("/api/mail", mailRouter)
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../client", "dist")));
     app.use("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../client", "/dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
     })
 }
 
